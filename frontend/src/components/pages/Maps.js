2ff0f2565bcca33179a93055311614425b325795
lingkai kong <lingkai2@illinois.edu>
2018-04-17 22:18:20 -0500

    update on map

--- frontend/src/components/pages/Maps.js
+++ frontend/src/components/pages/Maps.js
@@ -1,58 +1,92 @@
 import React, { Component } from 'react'
 import GoogleMapReact from 'google-map-react'
 
-const AnyReactComponent = ({ text }) => <div>{text}</div>
-const MARKER_SIZE = 40
-const greatPlaceStyle = {
-  lat: 40.1138326,
-  lng: -88.2216972
-}
-
 class Map extends Component {
+  constructor(props) {
+    super(props)
+    this.curr_mark = []
+  }
   onGoogleApiLoaded = ({ map, maps }) => {
     this.map = map
     this.maps = maps
-    this.infowindow = new maps.InfoWindow()
-    var address = { lat: 40.1138326, lng: -88.2216972 }
-    var service = new maps.places.PlacesService(map)
-    service.nearbySearch(
-      {
-        location: address,
-        radius: 2000,
-        types: ['school']
-      },
-      this.callback
-    )
+    this.infowindow = new maps.InfoWindow({ maxWidth: 100 })
   }
-
-  // constructor(props) {
-  //   super(props)
-  // }
-
-  callback = (results, status) => {
-    for (var i = 0; i < results.length; i++) {
-      this.renderMarkers(results[i])
-    }
-  }
-
-  renderMarkers = place => {
+  renderMarkers_user = place => {
     let marker = new this.maps.Marker({
       position: {
-        lat: parseFloat(place.geometry.location.lat()),
-        lng: parseFloat(place.geometry.location.lng())
+        lat: parseFloat(place['lati']),
+        lng: parseFloat(place['longi'])
       },
       map: this.map,
       title: 'Hello World!'
     })
-
+    this.curr_mark.push(marker)
+    console.log(place['lati'])
     marker.addListener('click', () => {
-      this.infowindow.setContent('test')
+      this.infowindow.setContent(
+        'Name: ' +
+          place['name'] +
+          '\n' +
+          'Gender:' +
+          place['gender'] +
+          '\n' +
+          'Interest:' +
+          place['interest'] +
+          '\n'
+      )
       this.infowindow.open(this.map, marker)
     })
   }
 
+  renderMarkers_rest = place => {
+    let marker = new this.maps.Marker({
+      position: {
+        lat: parseFloat(place['lati']),
+        lng: parseFloat(place['longi'])
+      },
+      map: this.map,
+      title: 'Hello World!'
+    })
+    this.curr_mark.push(marker)
+    marker.addListener('click', () => {
+      this.infowindow.setContent(
+        'Name: ' +
+          place['name'] +
+          '\n' +
+          'Address:' +
+          place['address'] +
+          '\n' +
+          'Rating:' +
+          place['rating'] +
+          '\n' +
+          'Category:' +
+          place['categories']
+      )
+      this.infowindow.open(this.map, marker)
+    })
+  }
+
+  clearCurrMarker = mar => {
+    for (var i = 0; i < mar.length; i++) {
+      mar[i].setMap(null)
+    }
+  }
+
   render() {
     let defaultCenter = this.props.defaultCenter
+    var results = this.props.nearList
+    this.clearCurrMarker(this.curr_mark)
+    if (this.props.isNearUser) {
+      for (var i = 0; i < results.length; i++) {
+        console.log('hi')
+        this.renderMarkers_user(results[i])
+      }
+    } else {
+      for (var i = 0; i < results.length; i++) {
+        console.log('hi')
+        this.renderMarkers_rest(results[i])
+      }
+    }
     return (
       <div style={{ height: '100vh' }}>
         <GoogleMapReact
