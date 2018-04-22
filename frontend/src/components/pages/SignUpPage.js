2d7a38c26837dadb5e16bb6f3c95a47d3c22644a
yuyang <leon960809@gmail.com>
2018-04-22 12:38:44 -0500

    fix some button position

--- frontend/src/components/pages/SignUpPage.js
+++ frontend/src/components/pages/SignUpPage.js
@@ -1,19 +1,28 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import SignupForm from '../forms/SignupForm'
+import { Button } from 'semantic-ui-react'
 import PropTypes from 'prop-types'
 import axios from 'axios'
+import '../../styles/signup.css'
 
 class SignUpPage extends React.Component {
   submit = data => {
     this.props.history.push('/login')
   }
-
+  handleback = e => {
+    this.props.history.goBack()
+  }
   render() {
     return (
       <div>
-        <h1> SignUp Page</h1>
-        <SignupForm submit={this.submit} />
+        <Button onClick={this.handleback} className="back">
+          HomePage
+        </Button>
+        <div className="signup">
+          <h1> SignUp Page</h1>
+          <SignupForm submit={this.submit} />
+        </div>
       </div>
     )
   }
